19b126d3021da0c4e8e30bdf7df848b4f49c2175
frsyi <fahmarosyi@gmail.com>
2025-10-22 10:21:53 +0700

    feat(routes): add base route handler

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const allRoutes = require("./src/routes");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to ToGetHer API" });
 });
+
+app.use(allRoutes);
 
 module.exports = app;
