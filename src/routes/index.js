348b614bc0d9f6cbf50172b615fc460d20593227
frsyi <fahmarosyi@gmail.com>
2025-10-22 16:08:05 +0700

    feat: add auth router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const authRouter = require("./auth.route");
 const userRouter = require("./user.route");
 
+router.use("/auth", authRouter);
 router.use("/users", userRouter);
 
 module.exports = router;
