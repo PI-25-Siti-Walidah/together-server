f8bc5e077c45264285651a3967a4acd33d279984
frsyi <fahmarosyi@gmail.com>
2025-10-28 17:16:41 +0700

    feat: add kategori route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ const { verifyToken } = require("../middleware/auth.middleware");
 
 const authRouter = require("./auth.route");
 const userRouter = require("./user.route");
+const kategoriRouter = require("./kategori.route");
 
 router.use("/auth", authRouter);
 router.use("/users", verifyToken, userRouter);
+router.use("/kategori", verifyToken, kategoriRouter);
 
 module.exports = router;
